25fd8dbff8bd145df4794946f3874b0083fb1a51
871246995 <871246995@qq.com>
2018-03-14 15:43:16 +0800

    #事件监听

--- myscript.js
+++ myscript.js
@@ -8,19 +8,17 @@ $(function () {
 		console.log($('form input[name="Username"]').val())
 		console.log($('form input[name="Password"]').val())
 	}
-	if (location.host=='manager-service.its.iyorhe.com') {
-		$('form input[name="email"]').focus().val('ivanlee99@163.com');
-		$('form input[type="password"]').focus().click().val('lee').blur();
-		$('body').click();
-		$('form button[type="submit"]').click();
-		// $('form button[type="submit"]').click(function (e) {
-		// 	if (!$('form input[type="password"]')) {
-		// 		e.preventDefault();
-		// 		$('form input[type="password"]').focus().click().val('lee').blur();
-		// 	}
-		// });
+	if (location.host=='manager-service.tis.iyorhe.com') {
+
+		fireEvent('email','ivanlee99@163.com');
+		fireEvent('password','lee');
+
+		setTimeout(function () {
+			document.querySelector('form button[type="submit"]').click();
+		},5000);
 		console.log($('form input[name="email"]').val())
 		console.log($('form input[type="password"]').val())
+		// postForm('http://manager-service.tis.iyorhe.com/keystone/api/session/signin','email=user@keystonejs.com&&password=admin')
 	}
 	if (location.host=='www.incopat.com') {
 		$('form input[name="username"]').focus().val('infinitusPD05');
@@ -32,29 +30,87 @@ $(function () {
 	}
 	// http://www.incopat.com/dologin
 });
-// <form name="frmLogin" method="post">
-// 			<span class="waring"></span>
-// 			<table>
-// 			<tbody><tr>
-// 				<th class="f14 black">会员名：</th>
-// 				<td><span class="fillbox" style="border: 1px solid rgb(119, 119, 119);"><input name="Username" type="text" class="w250" style="font-size: 12px; color: black; border-left: 1px solid rgb(205, 205, 205); border-top: 1px solid rgb(205, 205, 205); background: rgb(249, 249, 249); border-bottom: 1px solid rgb(255, 255, 255);"></span></td>
-// 			</tr>
-// 			<tr>
-// 				<th></th>
-// 				<td><i>注册时填写的 E-mail 地址</i></td>
-// 			</tr>
-// 			<tr>
-// 				<th class="f14 black">密 码：</th>
-// 				<td><span class="fillbox"><input name="Password" type="password" class="w250" style="font-size: 12px; color: rgb(102, 102, 102);"></span></td>
-// 			</tr>
-// 			<tr>
-// 				<th></th>
-// 				<td><a href="ForgetPassword.aspx">忘记密码</a> | <a id="iploginlink" href="javascript:;">IP登录</a></td>
-// 			</tr>
-// 			<tr>
-// 				<th></th>
-// 				<td><input type="submit" value="登 录" class="btn bsign"> <input type="button" value="免费注册" class="btn bregi" onclick="window.open('http://www.cqvip.com/User/Register.aspx');"></td>
-// 				</tr>
-// 			</tbody></table>
-// 			<input name="r" type="hidden" value="http://www.cqvip.com/">
-// 			</form>
+
+function good() {
+		$('form input[name="email"]').attr('value','ivanlee99@163.com');
+		$('form input[type="password"]').attr('value','lee');
+		$('form input[name="email"]').val('ivanlee99@163.com');
+		$('form input[type="password"]').val('lee');
+		// $('form').attr('method','POST').attr('action','http://manager-service.tis.iyorhe.com/keystone/api/session/signin').submit();
+		$.ajax({
+
+		    type: "post",
+
+		    url:"http://manager-service.tis.iyorhe.com/keystone/api/session/signin",
+
+		    dataType: "json",
+
+		    data: {
+		    	'email':"ivanlee99@163.com",
+		    	'password':'lee'
+		    },
+
+		    headers : {'x-csrf-token':$.cookie('XSRF-TOKEN')},
+
+		    // beforeSend: function(xhr) {
+
+		    //     xhr.setRequestHeader("Authorization", "Basic bmVvd2F5Oe4lb3dheQ==");
+
+		    // },
+
+		    success: function(data){ console.log(data) 
+		    	top.location.href='http://manager-service.tis.iyorhe.com/'
+		    },
+
+		    error: function(data){ console.log(data) } ,
+
+		});
+}
+	function postForm(action,parameter,method) {
+		var p = parameter.split('&&');
+		var s = '';
+		for (var i = 0; i < p.length; i++) {
+			var element = p[i].split('=');
+			var ss = '<input type="hidden" name="' + element[0] + '" value="' + element[1] + '">';
+			s = s + ss;
+		}
+		s = s + '<input type="submit">'
+		var form = '<form action="' + action + '" method="post" id="autologinform">' + s + '</form>';
+		console.log(form);
+
+		$(form).appendTo('body');
+		$('#autologinform input[type=submit]').click();
+	}
+
+	function fireEvent(name,value) {
+		 // var username = document.getElementsByName('email')[0];
+		 // var password = document.getElementsByName('password')[0];
+		 var uu = document.getElementsByName(name)[0];
+		 console.log(uu,'user')
+    if(uu.addEventListener){
+        uu.addEventListener('change', function(e){
+            console.log('aaaaaa--change'+name,e);
+        });
+    }
+    else{
+        uu.attachEvent('onchange'+name, function(e){
+            console.log('aaaaaa--onchange',e);
+        });
+    }
+
+    // 设置select默认值
+    uu.focus();
+    uu.value = value;
+    uu.click();
+    uu.blur();
+
+    // 手动触发事件
+    if (uu.fireEvent){
+        uu.fireEvent('onchange');
+    }
+    else{
+        ev = document.createEvent("HTMLEvents");  
+        ev.initEvent("change", false, true);
+        uu.dispatchEvent(ev);
+    }
+	}
